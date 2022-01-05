0ef78e6f5e7b80e41a14cf6e75fcf449f4343a9f
Piyush Singh <iamdeveloper0529@gmail.com>
2022-01-05 23:34:04 +0530

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='allauth',
+    version="0.40.0",
+
+    url='https://github.com/pysingh-dev/allauth',
+    author='Piyush Singh',
+    author_email='iamdeveloper0529@gmail.com',
+
+    py_modules=['allauth'],
+)
